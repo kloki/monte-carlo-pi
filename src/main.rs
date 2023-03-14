55781ef426aa5d226b6f794dbdef9684c56bdb16
Koen Klinkers <k.klinkers@gmail.com>
2023-03-14 19:07:48 +0100

    optimized

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
 use rand::Rng;
 
 fn main() {
-    let height = 2.;
-    let width = 3.;
+    println!(" 🎰 🥧 {}", monte_carlo_run(100000000));
+}
 
-    let mut square_counter = 0;
+fn monte_carlo_run(iterations: usize) -> f64 {
     let mut circle_counter = 0;
-
     let mut rng = rand::thread_rng();
 
-    for _ in 0..100000000 {
-        let point = Point::new(rng.gen_range(0.0..width), rng.gen_range(0.0..height));
-        if point.hit_square() {
-            square_counter += 1;
-        } else if point.hit_circle() {
+    for _ in 0..iterations {
+        let x: f64 = rng.gen_range(0.0..2.0) - 1.;
+        let y: f64 = rng.gen_range(0.0..2.0) - 1.;
+        if (x * x + y * y).sqrt() < 1. {
             circle_counter += 1;
         }
     }
-
-    println!("🥧 {}", circle_counter as f64 / square_counter as f64);
-}
-
-pub struct Point {
-    pub x: f64,
-    pub y: f64,
-}
-
-impl Point {
-    pub fn new(x: f64, y: f64) -> Self {
-        Point { x, y }
-    }
-    pub fn hit_square(&self) -> bool {
-        self.x < 1.0 && self.y < 1.0
-    }
-    pub fn hit_circle(&self) -> bool {
-        let w = (2. - self.x).abs();
-        let h = (1. - self.y).abs();
-        (w * w + h * h).sqrt() < 1.0
-    }
+    circle_counter as f64 / (iterations as f64 / 4.)
 }
