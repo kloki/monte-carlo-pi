f4bdc942fca6b988450fdf2dc2691fd65c91afb8
Koen Klinkers <k.klinkers@gmail.com>
2023-03-14 17:43:30 +0100

    sequantial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let height = 2.;
+    let width = 3.;
+
+    let mut square_counter = 0;
+    let mut circle_counter = 0;
+
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..100000000 {
+        let point = Point::new(rng.gen_range(0.0..width), rng.gen_range(0.0..height));
+        if point.hit_square() {
+            square_counter += 1;
+        } else if point.hit_circle() {
+            circle_counter += 1;
+        }
+    }
+
+    println!("🥧 {}", circle_counter as f64 / square_counter as f64);
+}
+
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Point {
+    pub fn new(x: f64, y: f64) -> Self {
+        Point { x, y }
+    }
+    pub fn hit_square(&self) -> bool {
+        self.x < 1.0 && self.y < 1.0
+    }
+    pub fn hit_circle(&self) -> bool {
+        let w = (2. - self.x).abs();
+        let h = (1. - self.y).abs();
+        (w * w + h * h).sqrt() < 1.0
+    }
 }
